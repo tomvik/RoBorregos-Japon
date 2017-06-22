e1d9ad8f943a266c1a993ff06c0fa818f935512c
Ernesto Cervantes <ernesto.acacej@gmail.com>
2017-06-22 18:02:52 -0500

    Nuevos test cases y control

--- Test/DCMotorTest/DCMotorTest.ino
+++ Test/DCMotorTest/DCMotorTest.ino
@@ -1,11 +1,20 @@
+#include <Arduino.h>
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
-Adafruit_DCMotor *mySecondMotor = AFMS.getMotor(4);
+Adafruit_DCMotor *alfa = AFMS.getMotor(1);// DERECHA ATRAS
+Adafruit_DCMotor *beta = AFMS.getMotor(2);
+Adafruit_DCMotor *ce = AFMS.getMotor(3);
+Adafruit_DCMotor *de = AFMS.getMotor(4);
 
+
+#define ENCODER_A 4
+volatile int o = 0;
+void x() {
+  o++;
+}
 
 void setup() {
   Serial.begin(9600);
@@ -13,65 +22,21 @@ void setup() {
   AFMS.begin();  // create with the default frequency 1.6KHz
   //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
 
-  myMotor->setSpeed(150);
-  myMotor->run(FORWARD);
-  myMotor->run(RELEASE);
+  alfa->setSpeed(155);
+  beta->setSpeed(155);
+  ce->setSpeed(155);
+  de->setSpeed(155);
+  alfa->run(FORWARD);// derecha atras
+  delay(2000);
+  beta->run(FORWARD); //IZQ ADEL
+  delay(2000);
+  ce->run(BACKWARD); //izq atraz
+  delay(2000);
+  de->run(FORWARD);// DERECHA ADE
+  attachInterrupt(ENCODER_A, x, RISING);
 }
 
 void loop() {
-  uint8_t i;
-
-  Serial.print("tick");
-
-  myMotor->run(FORWARD);
-  for (i=0; i<255; i++) {
-    myMotor->setSpeed(i);
-    delay(10);
-  }
-  for (i=255; i!=0; i--) {
-    myMotor->setSpeed(i);
-    delay(10);
-  }
-
-  Serial.print("tock");
-
-  myMotor->run(BACKWARD);
-  for (i=0; i<255; i++) {
-    myMotor->setSpeed(i);
-    delay(10);
-  }
-  for (i=255; i!=0; i--) {
-    myMotor->setSpeed(i);
-    delay(10);
-  }
-
-  Serial.print("tech");
-  myMotor->run(RELEASE);
-  delay(1000);
-
-  mySecondMotor->run(FORWARD);
-  for (i=0; i<255; i++) {
-    mySecondMotor->setSpeed(i);
-    delay(10);
-  }
-  for (i=255; i!=0; i--) {
-    mySecondMotor->setSpeed(i);
-    delay(10);
-  }
-
-  Serial.print("tock");
-
-  mySecondMotor->run(BACKWARD);
-  for (i=0; i<255; i++) {
-    mySecondMotor->setSpeed(i);
-    delay(10);
-  }
-  for (i=255; i!=0; i--) {
-    mySecondMotor->setSpeed(i);
-    delay(10);
-  }
-
-  Serial.print("tech");
-  mySecondMotor->run(RELEASE);
-  delay(1000);
+  Serial.println(o);
+  delay(100);
 }
